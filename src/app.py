cd8f1d5ef0c3fabaee67693ea3ec709c37342bca
Vladyslav Shcherbak <vladserbak890@gmail.com>
2024-11-22 21:07:25 +0000

    Made all getters for Purchase class and setter for quantity

--- src/app.py
+++ src/app.py
@@ -1,19 +1,43 @@
 from datetime import datetime as dt
 current_date = dt.now
 
+
 class Purchase:
     def __init__(self, purchase_name: str, price: float, category: str,
                  purchase_date=str(current_date()), quantity=1):
-        self.purchase_name = purchase_name
-        self.price = price
-        self.category = category
-        self.purchase_date = purchase_date
-        self.quantity = quantity
+        self.__purchase_name = purchase_name
+        self.__price = price
+        self.__category = category
+        self.__purchase_date = purchase_date
+        self.__quantity = quantity
+
+
+    def get_purchase_name(self):
+        return self.__purchase_name
+
+
+    def get_price(self):
+        return self.__price
+
+
+    def get_category(self):
+        return self.__category
+
+
+    def get_purchase_date(self):
+        return self.__purchase_date
+
+
+    def get_quantity(self):
+        return self.__quantity
+
+
+    def set_quantity(self, new_quantity):
+        if new_quantity > 0:
+            self.__quantity = new_quantity
+
 
     def __str__(self):
-        return (f"{__name__}: name= {self.purchase_name}, price= {self.price},"
-                f" category={self.category}, purchase_date= {self.purchase_date},"
-                f" quantity= {self.quantity}")
-
-first_purchase = Purchase("beer", 6, "alcohol")
-print(first_purchase)
+        return (f"name= {self.__purchase_name}, price= {self.__price},"
+                f" category={self.__category}, purchase_date= {self.__purchase_date},"
+                f" quantity= {self.__quantity}")
