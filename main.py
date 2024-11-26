ba2468974916d2059b49c44dbdc0ebc814302a28
Vladyslav Shcherbak <vladserbak890@gmail.com>
2024-11-26 22:28:06 +0000

    Added function main().

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from src.app import Purchase
 
-first_purchase = Purchase("beer", 6, "alcohol")
-# print(first_purchase)
-# print(first_purchase.get_purchase_name())
-# print(first_purchase.get_price())
-# print(first_purchase.get_purchase_date())
-# print(first_purchase.get_category())
-# print(first_purchase.get_quantity())
-# first_purchase.set_quantity(2)
-# print(first_purchase.get_quantity())
-first_purchase.increase_quantiy(0)
-print(first_purchase.display_purchase())
+def main():
+    first_purchase = Purchase("beer", 6, "alcohol")
+    # print(first_purchase)
+    # print(first_purchase.get_purchase_name())
+    # print(first_purchase.get_price())
+    # print(first_purchase.get_purchase_date())
+    # print(first_purchase.get_category())
+    # print(first_purchase.get_quantity())
+    # first_purchase.set_quantity(2)
+    # print(first_purchase.get_quantity())
+    print("Program start")
+    first_purchase.increase_quantiy()
+    print(first_purchase.display_purchase())
+
+if __name__ == "__main__":
+    main()
